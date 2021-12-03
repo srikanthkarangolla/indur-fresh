ba1e1ed04fb1abdc39690065c9845ddbfd431948
gopikrishnanayini <nayinigopi93@gmail.com>
2021-12-03 22:47:26 +0530

    changes

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,4 +2,13 @@ class HomesController < ApplicationController
 
 	def index
 	end
+
+	def about
+	end
+
+	def contacts
+	end
+
+	def services
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'homes#index'
+
+  get 'about', to: 'homes#about'
+  get 'services', to: 'homes#services'
+  get 'contacts', to: 'homes#contacts'
+  get 'products', to: 'homes#products'
 end
