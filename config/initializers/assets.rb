74bcbe53f60f8c845d4e2596a4a8b6a2c76f275a
gopikrishnanayini <nayinigopi93@gmail.com>
2021-12-07 09:23:39 +0530

    assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,5 +12,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w( bootstrap.min.js bxslider.js jquery-ui.js jquery.bootstrap-touchspin.js jquery.fancybox.js jquery.js map-helper.js appear.js nav-tool.js owl.js pagenav.js popper.min.js script.js scrollbar.js validation.js wow.js 
- animate.css bootstrap.css color.css flaticon.css font-awesome-all.css jquery-ui.css jquery.bootstrap-touchspin.css jquery.fancybox.min.css own.css responsive.css rtl.css style.css )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js bxslider.js jquery-ui.js jquery.fancybox.js jquery.js map-helper.js appear.js nav-tool.js owl.js pagenav.js popper.min.js script.js scrollbar.js validation.js wow.js 
+ animate.css bootstrap.css color.css flaticon.css font-awesome-all.css jquery-ui.css jquery.fancybox.min.css own.css responsive.css rtl.css style.css )
